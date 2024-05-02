79014cdbe6cc71d43bd3cfa8f88f1e13a5fa3c08
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-05-03 01:51:25 +0200

    Rename parameters

--- src/pynguin/plugins/grammar_fuzzer/csv.py
+++ src/pynguin/plugins/grammar_fuzzer/csv.py
@@ -18,30 +18,30 @@ from pynguin.plugins.grammar_fuzzer.grammar import Sequence
 
 
 def create_csv_grammar(  # noqa: PLR0917
-    column_number: int,
+    columns_number: int,
     min_field_length: int = 0,
     max_field_length: int | None = 10,
-    min_row_number: int = 1,
-    max_row_number: int | None = 10,
+    min_rows_number: int = 1,
+    max_rows_number: int | None = 10,
     string_constants: list[str] | None = None,
 ) -> Grammar:
     """Create a grammar for CSV files.
 
     Args:
-        column_number: The number of columns in the CSV file.
+        columns_number: The number of columns in the CSV file.
         min_field_length: The minimum length of a field.
         max_field_length: The maximum length of a field.
-        min_row_number: The minimum number of rows in the CSV file.
-        max_row_number: The maximum number of rows in the CSV file.
+        min_rows_number: The minimum number of rows in the CSV file.
+        max_rows_number: The maximum number of rows in the CSV file.
         string_constants: A list of string constants to use in the CSV file.
 
     Returns:
         Grammar: A grammar for CSV files.
     """
-    assert column_number > 0
+    assert columns_number > 0
 
     rules: list[GrammarRule] = [RuleReference("field")]
-    for _ in range(column_number - 1):
+    for _ in range(columns_number - 1):
         rules.extend((Constant(","), RuleReference("field")))
     rules.append(Constant("\n"))
 
@@ -56,7 +56,7 @@ def create_csv_grammar(  # noqa: PLR0917
     return Grammar(
         "csv",
         frozendict(
-            csv=Repeat(RuleReference("row"), min=min_row_number, max=max_row_number),
+            csv=Repeat(RuleReference("row"), min=min_rows_number, max=max_rows_number),
             row=Sequence(tuple(rules)),
             field=Choice(
                 rules=(

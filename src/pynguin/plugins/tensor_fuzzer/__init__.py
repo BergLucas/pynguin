bd369c191ffca85510707d52be263105d1f61940
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-05-24 16:26:38 +0200

    Moving tensor plugin

---
+++ src/pynguin/plugins/tensor_fuzzer/__init__.py
@@ -0,0 +1,7 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2024 Pynguin Contributors
+#
+#  SPDX-License-Identifier: MIT
+#
+"""Provides classes and plugins to generate test cases data based on tensors."""
